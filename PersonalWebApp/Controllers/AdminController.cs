4a99c2cc0cf7991d795235887858e7d56a4fdcb7
Richard Gray <gray.richardc@gmail.com>
2023-04-24 23:30:17 +1200

    Admin page lists users and can give/remove permissions

--- PersonalWebApp/Controllers/AdminController.cs
+++ PersonalWebApp/Controllers/AdminController.cs
@@ -1,14 +1,65 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalWebApp.Models;
+using System.Security.Claims;
 
 namespace PersonalWebApp.Controllers
 {
     [Authorize(Policy ="RequireAdminRole")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var model = new List<AdminViewModel>();
+
+            foreach (var user in users)
+            {
+                var claims = await _userManager.GetClaimsAsync(user);
+                var canCreateBlogPost = claims.Any(c => c.Type == "CanCreateBlogPosts");
+                model.Add(new AdminViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    CanCreateBlogPost = canCreateBlogPost
+                });
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateCreateBlogPostClaim(string userID, bool canCreateBlogPost)
+        {
+            var user = await _userManager.FindByIdAsync(userID);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            if (canCreateBlogPost)
+            {
+                await _userManager.AddClaimAsync(user, new Claim("CanWriteBlogPosts", "true"));
+            }
+            else
+            {
+                var claim = (await _userManager.GetClaimsAsync(user)).FirstOrDefault(c => c.Type == "CanWriteBlogPosts");
+                if (claim == null)
+                {
+                    await _userManager.RemoveClaimAsync(user, claim);
+                }
+            }
+
+            return Ok();
         }
     }
 }
---
+++ PersonalWebApp/Models/AdminViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalWebApp.Models
+{
+    public class AdminViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public bool CanCreateBlogPost { get; set; }
+    }
+}
